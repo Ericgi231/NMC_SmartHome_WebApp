5d99601604457668d22624070aafb6686d4c6db8
fongle support <support@fongle.info>
2019-03-12 12:39:29 -0400

    fixed nav bar

--- index.php
+++ index.php
@@ -8,20 +8,27 @@ ini_set('display_errors', 1);
 include 'php/connect.php';
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Intelligent Fongle Smart Home System</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<title>Dashboard</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<!-- Jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	
+	<!-- Custom CSS & Js-->
 	<link rel="stylesheet" href="css/site.css"/>	
 	<script src="js/main.js"></script>
 </head>
-<body style="font-family:Tahoma, Geneva, sans-serif;color:#aaaaaa;">
-
+<body>
+	<!-- Load navbar -->
 	<div id="nav-frame">
 	
 	</div>
-
+	
 	<h1>Welcome!</h1>
 <!-- Div code for Temperature Overview around this PHP block-->
         <div class="temp">
@@ -32,7 +39,7 @@ include 'php/connect.php';
 
 		if (mysqli_num_rows($result) > 0){
 			while ($row = mysqli_fetch_assoc($result)) {
-				echo "<p>Time: " . date_format(date_create($row["RecordTime"]), "m/d/y-h:i A") . "</p>";
+				//echo "<p>Time: " . date_format(date_create($row["RecordTime"]), "m/d/y-h:i A") . "</p>";
 				echo "<p>Humidity: " . $row["Humidity"] . "%</p>";
 				echo "<p>Temperature: " . (($row["Temperature"]*9/5) + 32) . "*f</p>";
 				echo "<br/>";
@@ -50,19 +57,6 @@ include 'php/connect.php';
 
 	 </div>
 
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-	 </br>
-
 	 <div class="sensor">
         <h2>Sensor Overview</h2>       
 	 </div>
@@ -71,13 +65,12 @@ include 'php/connect.php';
 		<h2>Alarm Overview</h2>       
 	 </div>
 
-	 </br>
-	 </br>
-	 </br>
-	 </br>
 	  <footer>
 		<div style="background-color:#00BCD5;color:#000000;text-align:center;padding:10px;margin-top:7px;text-align:center;">© Intelligent Fongle Smart Home System</div>
 	  </footer>
-
+	
+	<!-- Bootstrap Js -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
