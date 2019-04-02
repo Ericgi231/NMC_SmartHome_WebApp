1dd6c21abfc71aaa60471d2cca79f6503cdd0ae0
pipboy2077 <pipboy2077@gmail.com>
2019-04-02 09:46:10 -0400

    Added Script to Climate for Chart.js

--- climate.php
+++ climate.php
@@ -19,7 +19,7 @@
 
 	<!-- Jquery -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.js"></script>
 	<!-- Custom CSS & Js-->
 	<link rel="stylesheet" href="css/site.css"/>	
 	<link rel="stylesheet" href="css/index.css"/>
